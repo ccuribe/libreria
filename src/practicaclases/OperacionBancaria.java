4ad02881ec7c2bd335c563985387b9f1a640eb6f
Cristian C Uribe <ccuribe@yahoo.com>
2013-06-23 18:43:16 -0430

    Primera Publicación
    Primera Publicación con las clases del ejercicio con sus atributos. No
    hay relación aún entre ellas

---
+++ src/practicaclases/OperacionBancaria.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaclases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Cristian
+ */
+public class OperacionBancaria {
+    String ClaveOperacion;
+    Date fechaOperacion;
+    long idOperacion;
+}
---
+++ src/practicaclases/TipoOperacion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicaclases;
+
+/**
+ *
+ * @author Cristian
+ */
+public class TipoOperacion {
+    String descripcion;
+    long idTipoOperacion;
+}
