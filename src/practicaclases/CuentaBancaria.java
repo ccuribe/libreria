95bed2a9ff262e7c0607b918ae3c62301037766e
Armando <Armando@192.168.1.140>
2013-06-27 18:45:07 -0430

    Revision de clases

--- src/practicaclases/CuentaBancaria.java
+++ src/practicaclases/CuentaBancaria.java
@@ -4,10 +4,22 @@
  */
 package practicaclases;
 
+import java.util.Date;
+
 /**
  *
  * @author Cristian
  */
 public class CuentaBancaria {
-    
+ private Date fechaApertura;
+ private long idCuentaBancaria;
+ private String numeroCuenta;
+ 
+ public void createCuentaBancaria(){
+     
+ }
+ 
+ public void getCuentaBancariaByID(long Id1){
+     
+ }
 }
