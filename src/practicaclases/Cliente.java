5d562725d99e97ccfa9eead4b776ff90404dc82f
Ender Mendez <ing.endermendez@gmail.com>
2013-06-29 01:21:29 -0430

    Update Cliente.java

--- src/practicaclases/Cliente.java
+++ src/practicaclases/Cliente.java
@@ -3,14 +3,22 @@
  * and open the template in the editor.
  */
 package practicaclases;
-
+import java.io.*;
 /**
  *
  * @author Cristian
  */
 public class Cliente {
     private long idCliente;
-    private String nombreCliente;
+    private String[] nombreCliente = new String[50];
+    static int numCliente=1;
+    int cont;
+
+    public Cliente(String nombCli){
+          nombreCliente[numCliente] = nombCli;
+          idCliente = numCliente;
+          numCliente ++;
+    }
     
     public void createCliente(){
         
