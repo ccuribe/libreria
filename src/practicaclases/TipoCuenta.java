55c99d1e2e7c41e462b669a19a64f3c633e5a600
Ender Mendez <ing.endermendez@gmail.com>
2013-06-30 19:35:34 -0430

    Update TipoCuenta.java

--- src/practicaclases/TipoCuenta.java
+++ src/practicaclases/TipoCuenta.java
@@ -4,15 +4,49 @@
  */
 package practicaclases;
 
-/**
- *
- * @author Cristian
- */
-public class TipoCuenta {
-    private String descripcion;
-    private long idTipocuenta;
-    
-    public void addtipoCuenta(){
-        
+import java.io.*;
+import java.util.logging.*;
+
+
+public class TipoCuenta extends CuentaBancaria
+
+{
+    private long idTipoCuenta;
+    private String[] descripcion = new String[50];
+    static int numCuenta=0;
+    int cont;
+    public TipoCuenta()
+    {
+    }
+    public void addTipoCuenta() throws IOException
+    {
+        InputStreamReader entrada = new InputStreamReader(System.in);
+    BufferedReader teclado = new BufferedReader (entrada);
+	String nom;
+	System.out.println ("Ingrese descripción del tipo de cuenta:");
+	nom = teclado.readLine();
+	numCuenta ++;
+        descripcion[numCuenta] = nom;        
+        idTipoCuenta = numCuenta;
+        imprimir();
+    }
+    void imprimir()
+    {
+        if(numCuenta==0)
+        {
+            try {
+                addTipoCuenta();
+            } catch (IOException ex) {
+                Logger.getLogger(TipoCuenta.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        else
+        {
+            System.out.println ("\nListado de clientes");
+            for(cont=1;cont<=numCuenta;cont++)
+            {
+                System.out.println (cont+" "+descripcion[cont]);
+            }
+        }
     }
 }
